a8c12b78845889281eaa5e1ed4e8eb3ec454aee8
GeekGhc <ghcgavin@sina.com>
2019-06-09 21:52:27 +0800

    permutations

---
+++ src/medium/permutations.go
@@ -0,0 +1,32 @@
+package main
+
+import "fmt"
+
+func main()  {
+	arr := []int{1,2,3}
+	res := permute(arr)
+	fmt.Println("%v",res)
+}
+
+func permute(nums []int) [][]int{
+	res := make([][]int,0)
+	if len(nums) == 1{
+		res = append(res,nums)
+		return res
+	}
+
+	for i := range nums{
+		sub := make([]int,len(nums))
+		copy(sub,nums)
+
+		sub = append(sub[:i],sub[i+1:]...)
+		subR := permute(sub)
+		for _,sr := range subR {
+			rr := []int{nums[i]}
+			rr = append(rr, sr...)
+			res = append(res,rr)
+		}
+	}
+
+	return res
+}
