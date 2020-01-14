1c3ae076256f16dee5f143243a715d6e1ce1a005
GeekGhc <ghcgavin@sina.com>
2020-01-14 17:56:43 +0800

    unique-paths

--- medium/unique-paths.go
+++ medium/unique-paths.go
@@ -9,15 +9,29 @@ func main() {
 }
 
 func uniquePaths(m int, n int) int {
-	var arr = make([]int, m)
-	for k := range arr {
-		arr[k] = 1
-	}
+	//借助数组路径实现
+	//var arr = make([]int, m)
+	//for k := range arr {
+	//	arr[k] = 1
+	//}
+	//
+	//for i := 1; i < n; i++ {
+	//	for j := 1; j < m; j++ {
+	//		arr[j] += arr[j-1]
+	//	}
+	//}
+	//return arr[m-1]
 
-	for i := 1; i < n; i++ {
-		for j := 1; j < m; j++ {
-			arr[j] += arr[j-1]
+	//求解子问题
+	arr := [100][100]int{}
+	for i := 0; i < m; i++ {
+		for j := 0; j < n; j++ {
+			if i == 0 || j == 0 {
+				arr[i][j] = 1
+			} else {
+				arr[i][j] = arr[i][j-1] + arr[i-1][j]
+			}
 		}
 	}
-	return arr[m-1]
+	return arr[m-1][n-1]
 }
