eaa349f9f1761afbb0c9ea8a9b87c79f48205825
huija <1150555483@qq.com>
2020-02-02 14:08:38 +0800

    kth largest
    1. 可以使用小顶堆, 用最大的k个数, 最后堆顶就是结果
    2. 直接使用标准库sort, 快如闪电

--- medium/kth-largest-element-in-an-array.go
+++ medium/kth-largest-element-in-an-array.go
@@ -1,7 +1,9 @@
 package main
 
 import (
+	"container/heap"
 	"fmt"
+	"sort"
 )
 
 func main() {
@@ -53,4 +55,50 @@ func headily(nums []int, currentIndex int) {
 		//继续比较
 		headily(nums, max)
 	}
+}
+
+// 使用小顶堆来实现, 小顶堆的Len保持为k
+func findKthLargest2(nums []int, k int) int {
+	n := IntHeap(nums[0:k])
+	heap.Init(&n)
+	var min = 0
+	for index := k; index < len(nums); index++ {
+		if min == 0 {
+			min = heap.Pop(&n).(int)
+		}
+		if min < nums[index] { // 比堆顶大再推入(最大的那k个数形成的小顶堆),否则不推
+			heap.Push(&n, nums[index])
+			min = 0
+		}
+	}
+	if min == 0 {
+		return heap.Pop(&n).(int)
+	}
+	return min
+}
+
+type IntHeap []int
+
+func (h IntHeap) Len() int { return len(h) }
+
+// <改成>就是大顶堆了
+func (h IntHeap) Less(i, j int) bool { return h[i] < h[j] }
+func (h IntHeap) Swap(i, j int)      { h[i], h[j] = h[j], h[i] }
+func (h *IntHeap) Push(x interface{}) {
+	// Push and Pop use pointer receivers because they modify the slice's length,
+	// not just its contents.
+	*h = append(*h, x.(int))
+}
+func (h *IntHeap) Pop() interface{} {
+	old := *h
+	n := len(old)
+	x := old[n-1]
+	*h = old[0 : n-1]
+	return x
+}
+
+// 直接使用标准库的排序方法
+func findKthLargest3(nums []int, k int) int {
+	sort.Ints(nums)
+	return nums[len(nums)-k]
 }
