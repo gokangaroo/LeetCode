84015df098d49bdb1b20e445bc5a646abd04cd34
huija <1150555483@qq.com>
2020-02-03 22:18:56 +0800

    Number of Islands
    use dfs instead of bfs.

--- medium/number-of-islands.go
+++ medium/number-of-islands.go
@@ -15,13 +15,49 @@ func main() {
 	fmt.Println(numIslands(grid))
 }
 
+func numIslands(grid [][]byte) int {
+	nr := len(grid)
+	if nr == 0 {
+		return 0
+	}
+	nc := len(grid[0])
+	if nc == 0 {
+		return 0
+	}
+
+	islands := 0
+	for r := 0; r < nr; r++ {
+		for c := 0; c < nc; c++ {
+			if grid[r][c] == '1' {
+				islands++
+				dfs2(grid, r, c)
+			}
+		}
+	}
+	return islands
+}
+
+func dfs2(grid [][]byte, r int, c int) {
+	nr := len(grid)
+	nc := len(grid[0])
+	if r < 0 || c < 0 || r >= nr || c >= nc || grid[r][c] == '0' {
+		return
+	}
+
+	grid[r][c] = '0'
+	dfs2(grid, r-1, c)
+	dfs2(grid, r+1, c)
+	dfs2(grid, r, c-1)
+	dfs2(grid, r, c+1)
+}
+
 // 使用一个struct来模型化问题.
 type node2 struct {
 	y int
 	x int
 }
 
-func numIslands(grid [][]byte) int {
+func numIslands2(grid [][]byte) int {
 	// 等价于找1的集合=>竖直水平表示是一座岛屿上的
 	// 岛屿定义一个head, 就是遍历头头, 最后head的数量就是岛屿数量
 	// head遍历过的地方就标记为已经遍历过了
