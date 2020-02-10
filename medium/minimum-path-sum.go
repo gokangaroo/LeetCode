2c2511b02e17ed5da4b910ba5f7d3b3dcd331e0e
GeekGhc <ghcgavin@sina.com>
2020-02-10 21:13:16 +0800

    minimum-path-sum 动态规划

--- medium/minimum-path-sum.go
+++ medium/minimum-path-sum.go
@@ -13,6 +13,31 @@ func main() {
 func minPathSum(grid [][]int) int {
 	nr := len(grid)
 	nc := len(grid[0])
+	for i := 0; i < nr; i++ {
+		for j := 0; j < nc; j++ {
+			if i == 0 && j == 0 {
+				continue
+			} else if i == 0 {
+				grid[i][j] = grid[i][j-1] + grid[i][j]
+			} else if j == 0 {
+				grid[i][j] = grid[i-1][j] + grid[i][j]
+			} else {
+				if grid[i-1][j] < grid[i][j-1] {
+					//上面最小
+					grid[i][j] = grid[i-1][j] + grid[i][j]
+				} else {
+					//左边最小
+					grid[i][j] = grid[i][j-1] + grid[i][j]
+				}
+			}
+		}
+	}
+	return grid[nr-1][nc-1]
+}
+
+func minPathSumDfs(grid [][]int) int {
+	nr := len(grid)
+	nc := len(grid[0])
 
 	var path = make([][]int, nr)
 	for i := 0; i < nr; i++ {
