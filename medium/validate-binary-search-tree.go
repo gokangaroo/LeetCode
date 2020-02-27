ee8da30168d1c69e534d3c40d1acf6c3e5ff10bc
huija <1150555483@qq.com>
2020-02-27 23:32:53 +0800

    validate-binary-search-tree

--- medium/validate-binary-search-tree.go
+++ medium/validate-binary-search-tree.go
@@ -1,32 +1,55 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"math"
+)
+
+type TreeNode struct {
+	Val   int
+	Left  *TreeNode
+	Right *TreeNode
+}
 
 func main() {
-	root := &TreeNode{5, &TreeNode{1, nil, nil}, &TreeNode{9, &TreeNode{6, nil, nil}, &TreeNode{10, nil, nil}}}
+	root := &TreeNode{10, &TreeNode{5, nil, nil}, &TreeNode{15, &TreeNode{6, nil, nil}, &TreeNode{20, nil, nil}}}
 	fmt.Println(isValidBST(root))
 }
 
+//func isValidBST(root *TreeNode) bool {
+//	return dfs(root, nil, nil)
+//}
+//
+//func dfs(node *TreeNode, low, high *int) bool {
+//	if node == nil {
+//		return true
+//	}
+//	val := node.Val
+//	if low != nil && val <= *low {
+//		return false
+//	}
+//	if high != nil && val >= *high {
+//		return false
+//	}
+//	if !dfs(node.Right, &val, high) {
+//		return false
+//	}
+//	if !dfs(node.Left, low, &val) {
+//		return false
+//	}
+//	return true
+//}
+
 func isValidBST(root *TreeNode) bool {
-	return dfs(root, nil, nil)
+	return dfs(root, math.MinInt64, math.MaxInt64)
 }
 
-func dfs(node *TreeNode, low, high *int) bool {
-	if node == nil {
+func dfs(root *TreeNode, min, max int) bool {
+	if root == nil {
 		return true
 	}
-	val := node.Val
-	if low != nil && val <= *low {
+	if root.Val <= min || max <= root.Val {
 		return false
 	}
-	if high != nil && val >= *high {
-		return false
-	}
-	if !dfs(node.Right, &val, high) {
-		return false
-	}
-	if !dfs(node.Left, low, &val) {
-		return false
-	}
-	return true
+	return dfs(root.Left, min, root.Val) && dfs(root.Right, root.Val, max)
 }
