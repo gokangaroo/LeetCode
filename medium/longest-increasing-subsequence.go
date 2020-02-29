2fc25094358b738c4566d6f3e26028747aa4db4f
GeekGhc <ghcgavin@sina.com>
2020-02-29 22:39:59 +0800

    longest-increasing-subsequence

---
+++ medium/longest-increasing-subsequence.go
@@ -0,0 +1,37 @@
+package main
+
+import "fmt"
+
+func main() {
+	nums := []int{10, 9, 2, 5, 3, 7, 101, 18, 12, 13, 14, 15}
+	fmt.Print(lengthOfLIS(nums))
+}
+
+//经典最长子序列  二分插入
+func lengthOfLIS(nums []int) int {
+	length := len(nums)
+	tails := make([]int, length)
+	res := 0
+	for i := 0; i < length; i++ {
+		left := 0
+		right := res
+		for {
+			if left >= right {
+				break
+			}
+			mid := (left + right) / 2
+			if tails[mid] < nums[i] {
+				left = mid + 1
+			} else {
+				right = mid
+			}
+		}
+		//替换中间值
+		tails[left] = nums[i]
+		//最右边插入
+		if res == right {
+			res++
+		}
+	}
+	return res
+}
