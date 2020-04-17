193b88f68b2a34aa2f54dd59f0f60fffab61d1b2
huija <1150555483@qq.com>
2020-04-17 22:05:00 +0800

    postorder-traversal

--- hard/binary-tree-postorder-traversal.go
+++ hard/binary-tree-postorder-traversal.go
@@ -15,11 +15,26 @@ func main() {
 	fmt.Println(postorderTraversal2(x))
 }
 
-// TODO 循环本地栈解法.
+// 循环本地栈解法, 与前序遍历左右翻转, 上下翻转
 func postorderTraversal(root *TreeNode) []int {
 	res := make([]int, 0)
-	postorder(root, &res)
-	return res
+	if root == nil {
+		return res
+	}
+	stack := []*TreeNode{root}
+	var cur *TreeNode
+	for len(stack) != 0 {
+		cur = stack[len(stack)-1]
+		stack = stack[:len(stack)-1]
+		res = append(res, cur.Val)
+		// 与前序左右相反
+		if cur.Left != nil {
+			stack = append(stack, cur.Left)
+		}
+		if cur.Right != nil {
+			stack = append(stack, cur.Right)
+		}
+	}
 }
 
 func postorderTraversal2(root *TreeNode) []int {
