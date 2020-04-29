3ea5ddc552c8867ffa600c7f4bde3365bb129754
huija <1150555483@qq.com>
2020-04-29 20:28:15 +0800

    rm unused continue

--- easy/merge-sorted-array.go
+++ easy/merge-sorted-array.go
@@ -33,11 +33,9 @@ func merge(nums1 []int, m int, nums2 []int, n int) {
 		if nums1[i] > nums2[j] {
 			nums1[i+j+1] = nums1[i]
 			i--
-			continue
 		} else {
 			nums1[i+j+1] = nums2[j]
 			j--
-			continue
 		}
 	}
 }
