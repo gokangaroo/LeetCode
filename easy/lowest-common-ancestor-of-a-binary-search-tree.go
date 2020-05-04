7ad2ed905ded88084a39f0c733729c343241cc1a
huija <1150555483@qq.com>
2020-05-04 12:24:32 +0800

    二叉搜索树可以额外剪枝, 去除部分额外场景

--- easy/lowest-common-ancestor-of-a-binary-search-tree.go
+++ easy/lowest-common-ancestor-of-a-binary-search-tree.go
@@ -4,6 +4,12 @@ import (
 	"fmt"
 )
 
+type TreeNode struct {
+	Val   int
+	Left  *TreeNode
+	Right *TreeNode
+}
+
 func main() {
 	//[6,2,8,0,4,7,9,null,null,3,5]
 	p := &TreeNode{2,
@@ -14,8 +20,6 @@ func main() {
 	x := &TreeNode{6, p, q}
 	fmt.Println(lowestCommonAncestor(x, p, q))
 }
-
-// TODO 使用二叉搜索树的特性, 可以尝试简单解决?
 
 func lowestCommonAncestor(root, p, q *TreeNode) *TreeNode {
 	aim := make(chan *TreeNode)
@@ -28,8 +32,14 @@ func dfsSons(node, p, q *TreeNode, aim chan *TreeNode) bool {
 	if node == nil {
 		return false
 	}
-	left := dfsSons(node.Left, p, q, aim)
-	right := dfsSons(node.Right, p, q, aim)
+	var left, right bool
+	// 0.二叉搜索树,可以进行一定剪枝
+	if !(node.Val < p.Val && node.Val < q.Val) {
+		right = dfsSons(node.Left, p, q, aim)
+	}
+	if !(node.Val > p.Val && node.Val > q.Val) {
+		left = dfsSons(node.Right, p, q, aim)
+	}
 	if node == p || node == q { //1.回溯 2.放到dfs后面,防止正好另一个是其子集
 		if left || right {
 			aim <- node
@@ -62,10 +72,10 @@ func lowestCommonAncestor2(root, p, q *TreeNode) *TreeNode {
 			qF = cur.path
 		}
 		// 2.1 入队
-		if cur.Left != nil {
+		if cur.Left != nil && !(cur.Val < p.Val && cur.Val < q.Val) {
 			queue = append(queue, &tmpNode{cur.Left, append(cur.path, true)})
 		}
-		if cur.Right != nil {
+		if cur.Right != nil && !(cur.Val > p.Val && cur.Val > q.Val) {
 			tmp := make([]bool, len(cur.path), cap(cur.path)) // 拷贝
 			copy(tmp, cur.path)                               //每个左侧一列共享一个path
 			queue = append(queue, &tmpNode{cur.Right, append(tmp, false)})
@@ -83,12 +93,6 @@ func lowestCommonAncestor2(root, p, q *TreeNode) *TreeNode {
 		}
 	}
 	return root
-}
-
-type TreeNode struct {
-	Val   int
-	Left  *TreeNode
-	Right *TreeNode
 }
 
 type tmpNode struct {
