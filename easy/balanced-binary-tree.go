babfe4ac289a6be9b952c8aa97d5cdb510936043
huija <1150555483@qq.com>
2020-05-10 20:22:58 +0800

    添加不需要异步回调的递归解法

--- easy/balanced-binary-tree.go
+++ easy/balanced-binary-tree.go
@@ -13,12 +13,12 @@ func main() {
 	//			&TreeNode{4, nil, nil}},
 	//		&TreeNode{3, nil, nil}},
 	//	&TreeNode{2, nil, nil}}
-	root := &TreeNode{2,
-		&TreeNode{1, nil, nil},
-		&TreeNode{4,
-			&TreeNode{3, nil, nil},
-			&TreeNode{5, nil,
-				&TreeNode{6, nil, nil}}}}
+	root := &TreeNode{1,
+		&TreeNode{2,
+			&TreeNode{3,
+				&TreeNode{4, nil, nil}, &TreeNode{4, nil, nil}},
+			&TreeNode{3, nil, nil}},
+		&TreeNode{2, nil, nil}}
 	fmt.Println(isBalanced(root))
 }
 
@@ -28,43 +28,76 @@ type TreeNode struct {
 	Right *TreeNode
 }
 
-func isBalanced(root *TreeNode) (res bool) {
+func isBalanced1(root *TreeNode) (res bool) {
 	if root == nil {
 		return true
 	}
-	defer x()
-	xxx(root, 0)
-	return true
+	c := make(chan bool)
+	go checkBalance(root, 0, c)
+	return <-c
 }
 
-func xxx(x *TreeNode, height float64) float64 {
+func checkBalance(x *TreeNode, height float64, c chan bool) float64 {
+	if height == 0 {
+		defer func() {
+			select {
+			case c <- true:
+			default:
+			}
+		}()
+	}
 	height++
 	if x.Left == nil && x.Right == nil {
 		return height
 	}
 	if x.Left == nil {
 		if x.Right.Left != nil || x.Right.Right != nil {
-			panic("x")
+			c <- false
 		}
 		return height + 1
 	}
 	if x.Right == nil {
 		if x.Left.Left != nil || x.Left.Right != nil {
-			panic("x")
+			c <- false
 		}
 		return height + 1
 	}
-	l := xxx(x.Left, height)
-	r := xxx(x.Right, height)
+	l := checkBalance(x.Left, height, c)
+	r := checkBalance(x.Right, height, c)
 	//fmt.Println(l, r)
 	if math.Abs(l-r) > 1 {
-		panic("x")
+		c <- false
 	}
 	return math.Max(l, r)
 }
 
-func x() {
-	if p := recover(); p != nil {
-		//fmt.Printf("%+v\n", p)
+func isBalanced(root *TreeNode) bool {
+	if root == nil {
+		return true
 	}
+	// 左右是平衡树, 且树深度差在1以内
+	res, _ := isBalancedNode(root)
+	return res
+}
+
+func isBalancedNode(node *TreeNode) (res bool, height int) {
+	if node == nil {
+		return true, 0
+	}
+	l, lh := isBalancedNode(node.Left)
+	r, rh := isBalancedNode(node.Right)
+	if !(l && r) {
+		return
+	}
+	if lh-rh < -1 || lh-rh > 1 {
+		return
+	}
+	return true, max(lh, rh) + 1
+}
+
+func max(x, y int) int {
+	if x > y {
+		return x
+	}
+	return y
 }
