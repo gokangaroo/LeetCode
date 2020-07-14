3a04f43f1a273f69f0de086e65994f96a8aeb753
huija <1150555483@qq.com>
2020-07-14 16:28:34 +0800

    Find Pivot Index

--- easy/find-pivot-index.go
+++ easy/find-pivot-index.go
@@ -1,1 +1,27 @@
 package main
+
+import "fmt"
+
+func main() {
+	fmt.Println(pivotIndex([]int{1, 7, 3, 6, 5, 6}))
+}
+
+func pivotIndex(nums []int) int {
+	if len(nums) <= 1 {
+		return -1
+	}
+	sumL, sumR := 0, 0
+	for i := 0; i < len(nums); i++ {
+		sumR += nums[i]
+	}
+	for i := 0; i < len(nums); i++ {
+		if i-1 >= 0 {
+			sumL += nums[i-1]
+		}
+		sumR -= nums[i]
+		if sumL == sumR {
+			return i
+		}
+	}
+	return -1
+}
