182f01e207723b191257bfa545376e8557a8f72e
huija <1150555483@qq.com>
2021-03-16 20:09:07 +0800

    Update spiral-matrix.go

--- medium/spiral-matrix.go
+++ medium/spiral-matrix.go
@@ -6,20 +6,58 @@ func main() {
 	var matrix = [][]int{
 		//{2},
 		//{3},
-		{1,2,3},
-		{4,5,6},
-		{7,8,9},
+		{1, 2, 3, 4},
+		{5, 6, 7, 8},
+		{9, 10, 11, 12},
 	}
-	res := spiralOrder(matrix)
+	res := spiralOrder1(matrix)
 	fmt.Println(res)
+}
+
+func spiralOrder1(matrix [][]int) (res []int) {
+	/*
+	   1 <= m, n <= 10
+	   // j++,i++,j--,i--
+	*/
+	var (
+		i, j  int
+		s, m  = 0, len(matrix[0]) - 1
+		t, n  = 1, len(matrix) - 1
+		count = (m + 1) * (n + 1)
+	)
+	res = append(res, matrix[0][0])
+	count--
+	for count > 0 {
+		for j < m && count > 0 {
+			j++
+			res = append(res, matrix[i][j])
+			count--
+		}
+		m--
+		for i < n && count > 0 {
+			i++
+			res = append(res, matrix[i][j])
+			count--
+		}
+		n--
+		for j > s && count > 0 {
+			j--
+			res = append(res, matrix[i][j])
+			count--
+		}
+		s++
+		for i > t && count > 0 {
+			i--
+			res = append(res, matrix[i][j])
+			count--
+		}
+		t++
+	}
+	return
 }
 
 func spiralOrder(matrix [][]int) []int {
 	var res []int
-
-	if len(matrix) == 0 {
-		return res
-	}
 
 	//初始化坐标和临界点
 	var x, y, xStart, yStart = 0, 0, 0, 0
